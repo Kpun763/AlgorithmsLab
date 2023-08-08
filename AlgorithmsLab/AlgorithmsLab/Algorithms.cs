2f26edc0597ad731642d2d6ea4cc691143f22399
Kyle Washington <kmw1993@msn.com>
2023-08-08 15:32:03 -0400

    Uploading task 1 and 2 complete

---
+++ AlgorithmsLab/AlgorithmsLab/Algorithms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLab
+{
+    internal class Algorithms
+    {
+        //The time complexity is O(1) constant time
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        //The time complexity is O(1) constant time
+
+        public static bool AllNumbersLessThan100(List<int> numbers)
+        {
+            foreach (int num in numbers)
+            {
+                if (num >= 100)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    
+}
--- AlgorithmsLab/AlgorithmsLab/Program.cs
+++ AlgorithmsLab/AlgorithmsLab/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 5;
+            bool isEven = Algorithms.IsEven(num);
+
+            Console.WriteLine($"{num} is even: {isEven}");
+
+            List<int> numberList = new List<int> { 20, 50, 63, 90};
+
+            bool allLessThan100 = Algorithms.AllNumbersLessThan100(numberList);
+
+            Console.WriteLine($"All numbers are less than 100: {allLessThan100}");
         }
+
+        
+       
+
     }
+
 }
