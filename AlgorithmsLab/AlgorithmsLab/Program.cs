3daaf0c1f36303d4a3a049daefac5130cf56bd90
Kyle Washington <kmw1993@msn.com>
2023-08-08 14:08:25 -0400

    Uploading test commit

---
+++ AlgorithmsLab/AlgorithmsLab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
